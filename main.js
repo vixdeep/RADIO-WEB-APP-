74bfcbfe0d5bb6525c9d2eae9f04865b92a2de09
vixdeep <76264847+vixdeep@users.noreply.github.com>
2022-11-27 11:29:09 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song=""
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -10,6 +14,26 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Has Been Initialized.');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristXY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = "+ rightWristY);
+    }
 }
 
 function draw(){
@@ -18,4 +42,6 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
